1e178d1ea325c4909ac7300fa469def70ca412a5
cypriius <44511639+cypriius@users.noreply.github.com>
2019-05-21 09:10:54 +0200

    Add files via upload

---
+++ microcommerce/src/main/java/com/ecommerce/microcommerce/model/Reclamation.java
@@ -0,0 +1,58 @@
+package com.ecommerce.microcommerce.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name="reclamation")
+public class Reclamation {
+	@Id
+	@GeneratedValue
+	private int id;
+	@NotBlank
+	private String titre;
+	@NotBlank
+	private String description;
+	@NotBlank
+	private int nombre_vote;
+	
+	
+	
+	
+	public Reclamation(int id, @NotBlank String titre, @NotBlank String description, @NotBlank int nombre_vote) {
+		super();
+		this.id = id;
+		this.titre = titre;
+		this.description = description;
+		this.nombre_vote = nombre_vote;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getNombre_vote() {
+		return nombre_vote;
+	}
+	public void setNombre_vote(int nombre_vote) {
+		this.nombre_vote = nombre_vote;
+	}
+	
+
+}
