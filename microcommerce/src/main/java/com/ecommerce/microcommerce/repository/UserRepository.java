f827bb54114a3d9487883f9c5c049c16320a1440
Pierre <33554755+PierreIsep@users.noreply.github.com>
2019-05-24 11:12:26 +0200

    delete

--- microcommerce/src/main/java/com/ecommerce/microcommerce/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.microcommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecommerce.microcommerce.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
